28b9b56de2a6639ade69a573cb1e9af1775a639f
Willem van Bergen <willem@vanbergen.org>
2009-08-22 23:10:04 +0200

    Improved specs for ordinal queries

--- spec/integration/ordinal_querying_spec.rb
+++ spec/integration/ordinal_querying_spec.rb
@@ -11,55 +11,97 @@ describe ScopedSearch, :search_for do
     ScopedSearch::Spec::Database.close_connection    
   end
   
-  context 'ordinal fields' do
+  context 'ordinal' do
     
     before(:all) do
       @class = ScopedSearch::Spec::Database.create_model(:int => :integer, 
         :timestamp => :datetime, :unindexed => :unindexed_integer)
-      
-      @class.create!(:int =>  9, :timestamp => Time.now, :unindexed => 10)
-      @class.create!(:int => 10, :timestamp => Time.now, :unindexed => 10)      
-      @class.create!(:int => 11, :timestamp => nil,      :unindexed => 10)      
     end
     
     after(:all) do
       ScopedSearch::Spec::Database.drop_model(@class)
     end
-  
-    it "should find the record with an exact integer match" do
-      @class.search_for('9').should have(1).item
+    
+    context 'integer field' do  
+      
+      before(:all) do
+        @record = @class.create!(:int =>  9, :timestamp => Time.now, :unindexed => 10)               
+      end
+      
+      after(:all) do
+        @record.destroy
+      end
+      
+      it "should find the record with an exact integer match" do
+        @class.search_for('9').should have(1).item
+      end
+    
+      it "should find the record with an exact integer match with an explicit operator" do
+        @class.search_for('= 9').should have(1).item
+      end    
+    
+      it "should find the record with an exact integer match with an explicit field name" do
+        @class.search_for('int = 9').should have(1).item
+      end    
+
+      it "should find the record with an exact integer match with an explicit field name" do
+        @class.search_for('int > 8').should have(1).item
+      end    
+      
+      it "should find the record with a grater than operator and explicit field" do
+        @class.search_for('int > 9').should have(0).item
+      end    
+      
+      it "should find the record with an >= operator with an implicit field name" do
+        @class.search_for('>= 9').should have(1).item
+      end    
+      
+      it "should not return the record if only one predicate is true and AND is used (by default)" do
+        @class.search_for('int <= 8, int > 8').should have(0).item
+      end    
+      
+      it "should return the record in only one predicate is true and OR is used as operator" do
+        @class.search_for('int <= 8 || int > 8').should have(1).item
+      end    
     end
     
-    it "should find the record with an exact integer match with an explicit operator" do
-      @class.search_for('= 9').should have(1).item
-    end    
-    
-    it "should find the record with an exact integer match with an explicit field name" do
-      @class.search_for('int = 9').should have(1).item
-    end    
-    
-    it "should only find the record with an exact integer match in the indexed column" do
-      @class.search_for('10').should have(1).item
+    context 'unindexed field' do
+      
+      before(:all) do
+        @record = @class.create!(:int =>  9, :timestamp => Time.now, :unindexed => 10)     
+      end
+
+      after(:all) do
+        @record.destroy
+      end
+      
+      it "searching in the non-index column should raise an error" do
+        lambda { @class.search_for('unindexed = 10') }.should raise_error
+      end
+      
+      it "searching for the value of the unindexed field should return nothing" do
+        @class.search_for('= 10').should have(0).item
+      end
     end
-    
-    it "should only find the only record with int == 11" do
-      @class.search_for('> 10').should have(1).item
-    end
-    
-    it "should only find the two record with int == [10, 11]" do
-      @class.search_for('>= 10').should have(2).item
-    end
-    
-    it "should find only two records with a timestamp set" do
-      @class.search_for('>= %s' % Date.today.strftime('%Y-%m-%d')).should have(2).item
-    end   
-    
-    it "should find no record with a timestamp in the past" do
-      @class.search_for('< %s' % Date.today.strftime('%Y-%m-%d')).should have(0).item
-    end     
-    
-    it "searching in the non-index column should raise an error" do
-      lambda { @class.search_for('unindexed = 10') }.should raise_error
+
+    context 'datetime field' do
+      
+      before(:all) do
+        @record = @class.create!(:int =>  9, :timestamp => Time.now, :unindexed => 10)
+        @record = @class.create!(:int =>  9, :timestamp => nil,      :unindexed => 10)
+      end
+
+      after(:all) do
+        @record.destroy
+      end
+      
+      it "should find the records with a timestamp set" do
+        @class.search_for('>= %s' % Date.today.strftime('%Y-%m-%d')).should have(1).item
+      end   
+      
+      it "should find no record with a timestamp in the past" do
+        @class.search_for('< %s' % Date.today.strftime('%Y-%m-%d')).should have(0).item
+      end    
     end    
 
   end
