abb25a5d21fa962bf8767a0b39f844fa2fe86e80
Willem van Bergen <willem@vanbergen.org>
2009-08-23 07:40:11 +0200

    Improved documentation and file loading in scoped_search.rb

--- lib/scoped_search.rb
+++ lib/scoped_search.rb
@@ -2,25 +2,27 @@ module ScopedSearch
       
   module ClassMethods
     
-    def self.extended(base) # :nodoc:
-      require 'scoped_search/query_language'
-      require 'scoped_search/query_builder'
-      require 'scoped_search/definition'      
-    end
-    
-    # Export the scoped_search method fo defining the search options
+    # Export the scoped_search method fo defining the search options.
+    # This method will create a definition instance for the class if it does not yet exist,
+    # and use the object as block argument and retun value.
     def scoped_search
       @scoped_search ||= ScopedSearch::Definition.new(self)
       yield(@scoped_search) if block_given?
       return @scoped_search
     end
 
-    # Backwards compatibility?
+    # Defines fields to search on using a syntax compatible with scoped_search 1.2
+    # TODO: improve backwards compatibility?
     def searchable_on(*fields)
       fields.each { |field| scoped_search.on(field) }
     end
   end
 end
+
+# Load all lib files
+require 'scoped_search/definition'      
+require 'scoped_search/query_language'
+require 'scoped_search/query_builder'
 
 # Import the search_on method in the ActiveReocrd::Base class
 ActiveRecord::Base.send(:extend, ScopedSearch::ClassMethods)
