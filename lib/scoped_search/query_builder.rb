258a6a5629b5031665ab9e0207590b2cf3949beb
Amos Benari <abenari@redhat.com>
2011-04-11 11:51:17 +0100

    allow short format for boolean fields. example: a query like 'foo = true' can be replaced with a query 'foo'

--- lib/scoped_search/query_builder.rb
+++ lib/scoped_search/query_builder.rb
@@ -315,6 +315,12 @@ module ScopedSearch
       # Defines the to_sql method for AST LeadNodes
       module LeafNode
         def to_sql(builder, definition, &block)
+          # for boolean fields allow a short format (example: for 'enabled = true' also allow 'enabled')
+          field = definition.field_by_name(value)
+          if field && field.set? && field.complete_value.values.include?(true)
+            key = field.complete_value.map{|k,v| k if v == true}.compact.first
+            return builder.set_test(field, :eq, key, &block)
+          end
           # Search keywords found without context, just search on all the default fields
           fragments = definition.default_fields_for(value).map do |field|
             builder.sql_test(field, field.default_operator, value,'', &block)
