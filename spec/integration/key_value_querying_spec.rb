e840781daa5d39cd8d991249b362d965a8fc13f5
Willem van Bergen <willem@vanbergen.org>
2012-09-02 10:27:39 -0400

    Do not use the Bar constant twice - rename to KeyValue

--- spec/integration/key_value_querying_spec.rb
+++ spec/integration/key_value_querying_spec.rb
@@ -20,11 +20,11 @@ require "#{File.dirname(__FILE__)}/../spec_helper"
           ActiveRecord::Migration.create_table(:keys) { |t| t.string :name }
           class ::Key < ActiveRecord::Base; has_many :facts; end
 
-          ActiveRecord::Migration.create_table(:facts) { |t| t.string :value; t.integer :key_id; t.integer :bar_id }
-          class ::Fact < ActiveRecord::Base; belongs_to :key; belongs_to :bar; end
+          ActiveRecord::Migration.create_table(:facts) { |t| t.string :value; t.integer :key_id; t.integer :key_value_id }
+          class ::Fact < ActiveRecord::Base; belongs_to :key; belongs_to :key_value; end
 
           # The class that will run the queries
-          ::Bar = ScopedSearch::RSpec::Database.create_model(:name => :string) do |klass|
+          ::KeyValue = ScopedSearch::RSpec::Database.create_model(:name => :string) do |klass|
             klass.has_many :facts
             klass.has_many :keys, :through => :facts
             klass.scoped_search :in => :facts, :on => :value, :rename => :facts, :in_key => :keys, :on_key => :name, :complete_value => true
@@ -34,58 +34,58 @@ require "#{File.dirname(__FILE__)}/../spec_helper"
           @key2 = Key.create!(:name => 'size')
 
 
-          @bar1 = Bar.create!(:name => 'bar')
-          @bar2 = Bar.create!(:name => 'barbary')
+          @kv1 = KeyValue.create!(:name => 'bar')
+          @kv2 = KeyValue.create!(:name => 'barbary')
 
-          Fact.create!(:value => 'green', :key => @key1, :bar => @bar1)
-          Fact.create!(:value => 'gold' , :key => @key1, :bar => @bar2)
-          Fact.create!(:value => '5'    , :key => @key2, :bar => @bar1)
+          Fact.create!(:value => 'green', :key => @key1, :key_value => @kv1)
+          Fact.create!(:value => 'gold' , :key => @key1, :key_value => @kv2)
+          Fact.create!(:value => '5'    , :key => @key2, :key_value => @kv1)
 
         end
 
         after(:all) do
-          ScopedSearch::RSpec::Database.drop_model(Bar)
+          ScopedSearch::RSpec::Database.drop_model(KeyValue)
           ScopedSearch::RSpec::Database.drop_model(Fact)
           ScopedSearch::RSpec::Database.drop_model(Key)
           Object.send :remove_const, :Fact
           Object.send :remove_const, :Key
-          Object.send :remove_const, :Bar
+          Object.send :remove_const, :KeyValue
         end
 
         it "should find all bars with a fact name color and fact value green" do
-          Bar.search_for('facts.color = green').should have(1).items
+          KeyValue.search_for('facts.color = green').should have(1).items
         end
 
          it "should find all bars with a fact name color and fact value gold" do
-          Bar.search_for('facts.color = gold').first.name.should eql('barbary')
+          KeyValue.search_for('facts.color = gold').first.name.should eql('barbary')
         end
 
         it "should find all bars with a fact name size and fact value 5" do
-            Bar.search_for('facts.size = 5').should have(1).items
+          KeyValue.search_for('facts.size = 5').should have(1).items
         end
 
         it "should find all bars with a fact color green and fact size 5" do
-          Bar.search_for('facts.color = green and facts.size = 5').should have(1).items
+          KeyValue.search_for('facts.color = green and facts.size = 5').should have(1).items
         end
 
         it "should find all bars with a fact color gold or green" do
-                  Bar.search_for('facts.color = gold or facts.color = green').should have(2).items
+          KeyValue.search_for('facts.color = gold or facts.color = green').should have(2).items
         end
 
         it "should find all bars that has size value" do
-          Bar.search_for('has facts.size').should have(1).items
+          KeyValue.search_for('has facts.size').should have(1).items
         end
 
         it "should find all bars that has color value" do
-          Bar.search_for('has facts.color').should have(2).items
+          KeyValue.search_for('has facts.color').should have(2).items
         end
 
         it "should complete facts names" do
-          Bar.complete_for('facts.').should have(2).items
+          KeyValue.complete_for('facts.').should have(2).items
         end
 
          it "should complete values for fact name = color" do
-          Bar.complete_for('facts.color = ').should have(2).items
+          KeyValue.complete_for('facts.color = ').should have(2).items
         end
 
       end
