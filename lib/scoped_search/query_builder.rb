fa7a18b85afbad3d515b9604f2a0137bdb03a988
Dominique d'Argent <dominique.dargent@me.com>
2013-09-18 00:58:43 +0200

    Add H2Adapter to make case insensitive LIKE work with H2 database.

--- lib/scoped_search/query_builder.rb
+++ lib/scoped_search/query_builder.rb
@@ -545,6 +545,20 @@ module ScopedSearch
         end
       end
     end
+
+    # The H2 adapter also requires some tweaks to make the case insensitive LIKE work.
+    class H2Adapter < ScopedSearch::QueryBuilder
+
+      # Cast column type to VARCHAR_IGNORECASE to force case-insensitive comparisons.
+      def sql_test(field, operator, value, lhs, &block)
+        if field.textual? && [:like, :unlike].include?(operator)
+          yield(:parameter, (value !~ /^\%|\*/ && value !~ /\%|\*$/) ? "%#{value}%" : value.tr_s('%*', '%'))
+          return "CAST(#{field.to_sql(operator, &block)} AS VARCHAR_IGNORECASE) #{self.sql_operator(operator, field)} ?"
+        end
+
+        super
+      end
+    end
   end
 
   # Include the modules into the corresponding classes
